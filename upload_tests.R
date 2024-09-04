a6d66cfad830a9425c0fae38b5902c4e28b43253
jennyd29 <jenny.davies29@nhs.net>
2024-09-04 13:09:06 +0100

    added monthly change

--- upload_tests.R
+++ upload_tests.R
@@ -87,34 +87,57 @@ check_contract <- contract %>%
   group_by(YEAR_MONTH) %>% 
   summarise(n_contracts = n_distinct(CONTRACT_NUMBER),
             total_uda_target = sum(CONTRACTED_UDA, na.rm = TRUE), 
-            total_uoa_target = sum(CONTRACTED_UOA, na.rm = TRUE))
+            total_uoa_target = sum(CONTRACTED_UOA, na.rm = TRUE)) %>% 
+  arrange(YEAR_MONTH) %>% 
+  mutate(monthly_contract_change = n_contracts - lag(n_contracts),
+         monthly_uda_change = total_uda_target - lag(total_uda_target),
+         monthly_uoa_change = total_uoa_target - lag(total_uoa_target))
 
 check_npp <- npp %>% 
+  filter(EXCLUDE_FROM_NPT == "N") %>% 
   group_by(YEAR_MONTH) %>% 
   summarise(n_contracts = n_distinct(CONTRACT_NUMBER),
             total_adult = sum(BAND1_ADULT_COUNT, BAND23_ADULT_COUNT, na.rm = TRUE), 
-            total_child = sum(BAND1_CHILD_COUNT, BAND23_CHILD_COUNT, na.rm = TRUE))
+            total_child = sum(BAND1_CHILD_COUNT, BAND23_CHILD_COUNT, na.rm = TRUE)) %>% 
+  arrange(YEAR_MONTH) %>% 
+  mutate(monthly_contract_change = n_contracts - lag(n_contracts),
+         monthly_adult_change = total_adult - lag(total_adult),
+         monthly_child_change = total_child - lag(total_child))
 
 check_uda <- uda %>% 
   group_by(YEAR_MONTH) %>% 
   summarise(n_contracts = n_distinct(CONTRACT_NUMBER), 
-            total_uda_delivered = sum(UDA_DELIVERED, na.rm = TRUE))
+            total_uda_delivered = sum(UDA_DELIVERED, na.rm = TRUE)) %>% 
+  arrange(YEAR_MONTH) %>% 
+  mutate(monthly_contract_change = n_contracts - lag(n_contracts),
+         monthly_uda_change = total_uda_delivered - lag(total_uda_delivered))
 
 check_dcp <- dcp %>% 
   group_by(YEAR_MONTH) %>% 
   summarise(n_contracts = n_distinct(CONTRACT_NUMBER), 
-            total_uda_delivered = sum(UDA_DELIVERED, na.rm = TRUE))
+            total_uda_delivered = sum(UDA_DELIVERED, na.rm = TRUE)) %>% 
+  arrange(YEAR_MONTH) %>% 
+  mutate(monthly_contract_change = n_contracts - lag(n_contracts),
+         monthly_uda_change = total_uda_delivered - lag(total_uda_delivered))
 
 check_uda_fd <- uda_fd %>% 
   group_by(YEAR_MONTH) %>% 
   summarise(n_contracts = n_distinct(CONTRACT_NUMBER), 
-            total_uda_delivered = sum(UDA_DELIVERED, na.rm = TRUE))
+            total_uda_delivered = sum(UDA_DELIVERED, na.rm = TRUE)) %>% 
+  arrange(YEAR_MONTH) %>% 
+  mutate(monthly_contract_change = n_contracts - lag(n_contracts),
+         monthly_uda_change = total_uda_delivered - lag(total_uda_delivered))
 
 check_unique <- unique %>% 
   group_by(YEAR_MONTH) %>% 
-  summarise(total_all = sum(ALL_12M_COUNT, na.rm = TRUE))
+  summarise(total_all = sum(ALL_12M_COUNT, na.rm = TRUE)) %>% 
+  arrange(YEAR_MONTH) %>% 
+  mutate(monthly_change = total_all - lag(total_all))
 
 check_uoa <- uoa %>% 
   group_by(YEAR_MONTH) %>% 
   summarise(n_contracts = n_distinct(CONTRACT_NUMBER), 
-            total_uoa_delivered = sum(UOA_DELIVERED, na.rm = TRUE))
+            total_uoa_delivered = sum(UOA_DELIVERED, na.rm = TRUE)) %>% 
+  arrange(YEAR_MONTH) %>% 
+  mutate(monthly_contract_change = n_contracts - lag(n_contracts),
+         monthly_uoa_change = total_uoa_delivered - lag(total_uoa_delivered))
